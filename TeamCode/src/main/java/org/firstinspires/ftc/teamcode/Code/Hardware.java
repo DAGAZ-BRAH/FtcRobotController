5d999339cc594484664799d4164fb90026c5bc36
SECRETYOGURT\kingf <kingfisher2004@outlook.com>
2021-03-10 11:02:02 -0500

    crab rotate be like be like rotate

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code/Hardware.java
@@ -2,9 +2,12 @@ package org.firstinspires.ftc.teamcode.Code;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Hardware {
     DcMotor backLeft, backRight, frontLeft, frontRight;
+    DcMotor crabMotor;
+    Servo crabServo;
     HardwareMap hwMap = null;
 
     public Hardware()
@@ -20,6 +23,13 @@ public class Hardware {
         backRight = hwMap.get(DcMotor.class, "backRight");
         frontLeft  = hwMap.get(DcMotor.class, "frontLeft");
         frontRight = hwMap.get(DcMotor.class, "frontRight");
+        crabMotor = hwMap.get(DcMotor.class, "crabMotor");
+        crabServo = hwMap.get(Servo.class, "crabServo");
+
+        backLeft.setPower(0);
+        backRight.setPower(0);
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
 
     }
 
