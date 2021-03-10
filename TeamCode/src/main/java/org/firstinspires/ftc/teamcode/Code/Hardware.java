5595bccd834cd2c2b065c75719265081a0333255
SECRETYOGURT\kingf <kingfisher2004@outlook.com>
2021-03-10 12:29:37 -0500

    Updated Hardware class includes carb parts

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code/Hardware.java
@@ -1,14 +1,15 @@
 package org.firstinspires.ftc.teamcode.Code;
 
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Hardware {
-    DcMotor backLeft, backRight, frontLeft, frontRight;
-    DcMotor crabMotor;
-    Servo crabServo;
+    DcMotor backLeft, backRight, frontLeft, frontRight, intakeMotor, crabMotor;
     HardwareMap hwMap = null;
+    CRServo slideFLeft, slideFRight, slideBLeft, slideBRight;
+    public Servo crab;
 
     public Hardware()
     {
@@ -24,12 +25,24 @@ public class Hardware {
         frontLeft  = hwMap.get(DcMotor.class, "frontLeft");
         frontRight = hwMap.get(DcMotor.class, "frontRight");
         crabMotor = hwMap.get(DcMotor.class, "crabMotor");
-        crabServo = hwMap.get(Servo.class, "crabServo");
 
+        //Define and initialize servos
+
+
+        //Set all motors to zero power.
         backLeft.setPower(0);
         backRight.setPower(0);
         frontLeft.setPower(0);
         frontRight.setPower(0);
+        crabMotor.setPower(0);
+
+        //Allows for motors to run without encoders, except for the front left motor.
+        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        crabMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
 
     }
 
